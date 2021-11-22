a32740872fd20b23992cf1e8241276cb5de99e37
Max Kobrow <max.kobrow@gmail.com>
2021-11-22 14:41:33 +0100

    removed data

--- CSVtoRAC2/Program.cs
+++ CSVtoRAC2/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json;
-using System.Configuration;
 using System.Globalization;
 using System.IO;
 using System.Text;
@@ -9,20 +8,6 @@ using System.Text;
 namespace CSVtoRAC2 {
     class Program {
         static void Main(string[] args) {
-            // boat testboat = new boat(1, "Max Kobrow");
-            // race_definition testrace = new race_definition("Rennen 1 Senioren A", "Re1 SMA", 
-            //     ConfigurationManager.AppSettings.Get("wettkampfname"),2000 , "1");
-            //
-            // testrace.addBoat(testboat);
-            //
-            // headerObject testobj = new headerObject(testrace);
-            //
-            // var options = new JsonSerializerOptions { WriteIndented = true, };
-            // var jsonString = JsonSerializer.Serialize<headerObject>(testobj, options);
-            //
-            // // File.WriteAllText("C:\\Users\\Max Kobrow\\RiderProjects\\CSVtoRAC2\\CSVtoRAC2\\test.rac2", jsonString);
-            // Console.Write(jsonString);
-
             csvReader csv = new csvReader(args[0]);
             List<headerObject> races = csv.GetRaces();
 
